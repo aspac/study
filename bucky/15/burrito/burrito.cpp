2867042d8b22f878b8eabc8ab2be6063f3d1aaef
dana satriya <dana_satriya@yahoo.com>
2016-01-12 20:51:49 +0100

    add project using codeblocks. burrito!

---
+++ bucky/15/burrito/burrito.cpp
@@ -0,0 +1,9 @@
+#include "burrito.h"
+#include <iostream>
+using namespace std;
+
+burrito::burrito()
+{
+    cout << "Ctor burrito!" << endl;
+}
+
---
+++ bucky/15/burrito/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "burrito.h"
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    burrito burrObj;
+
+    return 0;
+
+
+}
