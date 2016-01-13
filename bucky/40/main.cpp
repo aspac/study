8d44af3a46d95154d179b8d0bb567cc78e6e6c17
dana satriya <dana_satriya@yahoo.com>
2016-01-13 20:25:10 +0100

    Tutorial 40: sizeof

---
+++ bucky/40/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    char c;
+    cout << sizeof(c) << endl;
+
+    int d;
+    cout << sizeof(d) << endl;
+
+    double e;
+    cout << sizeof(e) << endl;
+
+    double bucky[10];
+    cout << sizeof(bucky);
+
+}
